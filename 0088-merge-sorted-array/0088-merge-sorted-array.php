70ead28273f572b70b2ceac70cda5e0328767ed8
JUHA KIM <88622675+hayamaster@users.noreply.github.com>
2025-05-16 11:23:46 +0900

    Time: 0 ms (100%), Space: 20.9 MB (14.35%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.php
@@ -0,0 +1,18 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums1
+     * @param Integer $m
+     * @param Integer[] $nums2
+     * @param Integer $n
+     * @return NULL
+     */
+    function merge(&$nums1, $m, $nums2, $n) {
+        $nums1 = array_splice($nums1, 0, $m); 
+        $nums2 = array_splice($nums2, 0, $n);
+        $nums1 = array_merge($nums1, $nums2);
+        sort($nums1);
+
+        return $nums1;
+    }
+}
